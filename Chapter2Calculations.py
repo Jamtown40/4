3f6dc7ee4f9642c0eb0ea9334015b5a374363b49
Ray Manzi <70032821+Jamtown40@users.noreply.github.com>
2022-12-01 13:23:55 -0500

    Add files via upload

---
+++ Chapter2Calculations.py
@@ -0,0 +1,46 @@
+# Author: Prof. Brian Candido
+# Purpose: Test your knowledge of Python
+#          data types and calculations
+
+# float division
+nAnswer = 9 / 2
+print("float division nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# integer division
+nAnswer = 9 // 2
+print("integer division nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# multiplication with 2 integers:
+nAnswer = 9 * 2
+print("multiplication with 2 ints nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# multiplication with 1 integer and 1 float:
+nAnswer = 9 * 2.0
+print("multiplication with 1 int and 1 float nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# multiplication with 2 floats:
+nAnswer = 9.0 * 2.0
+print("multiplication with 2 floats nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# multiplication with 2 floats and int conversion:
+nAnswer = int(9.0 * 2.0)
+print("multiplication with 2 floats and int() nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# integer division with 2 floats:
+nAnswer = 9.0 // 2.0
+print("integer division nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# integer division with 1 float and 1 int:
+nAnswer = 9 // 2.0
+print("integer division with 1 float and 1 int nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+# modulus division with 2 ints:
+nAnswer = 9 % 2
+print("modulus division with 2 ints nAnswer:", nAnswer, "Result data type is: ",type(nAnswer) )
+
+
+
+
+
+
+
