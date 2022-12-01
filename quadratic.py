d78befebe98857ac06d8b544b8e937a1b529fbe7
Ray M <70032821+Jamtown40@users.noreply.github.com>
2022-11-30 22:02:44 -0500

    Add files via upload

---
+++ quadratic.py
@@ -0,0 +1,18 @@
+# Declare the function:
+    
+def f(x):
+    return 3 * x ** 2 - 2 * x + 5
+
+# Use the function:
+
+nResult = f(3)
+print(nResult)
+
+# Or you can place the function call with in the print function:
+print( f(0) )
+
+print( f(1) )
+
+print( f(-1) )
+
+print( f(5) )
