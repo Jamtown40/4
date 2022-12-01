29019b5c0a4c4a7f21c11c84a4a9e09f46cebcff
Ray Manzi <70032821+Jamtown40@users.noreply.github.com>
2022-12-01 13:25:13 -0500

    Add files via upload

---
+++ Flooring.py
@@ -0,0 +1,28 @@
+# Flooring calculator
+
+# 1.Input both the Length and Width:
+
+--> Put Code Here
+--> Put Code Here
+
+# 2. Calculate
+# 2.1 Area is Length x Width
+
+--> Put Code Here
+
+# 2.2 Scrap is 10% of the calculated area:
+
+--> Put Code Here
+
+# 2.2 Total is Area + Scrap
+--> Put Code Here
+
+# 3. Output:
+#    Area of the room:
+#    Scrap value:
+#    Total of Area + Scrap:
+#    Total Square Yards which is Total / 9
+--> Put Code Here
+--> Put Code Here
+--> Put Code Here
+--> Put Code Here
