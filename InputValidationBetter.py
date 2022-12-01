d46e27a0688378404042c3ac12081b4b507ebda9
Ray M <70032821+Jamtown40@users.noreply.github.com>
2022-11-30 22:24:15 -0500

    Add files via upload

---
+++ InputValidationBetter.py
@@ -0,0 +1,9 @@
+# Sample of input a validation attempt
+# Better Approach
+
+# Get Test 1 score.
+iTest1 = 0
+# Keep asking until the user types in a number 0 or greater
+while iTest1 <= 0 : 
+ iTest1 = int(input("Test 1: "))
+
