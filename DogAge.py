67a3754803750f9eeb8c044cbd2c2cede5932c1a
Ray Manzi <70032821+Jamtown40@users.noreply.github.com>
2022-12-01 13:22:49 -0500

    Add files via upload

---
+++ DogAge.py
@@ -0,0 +1,16 @@
+# First Dog Age Calculator
+
+#1. Input:
+sAge = input("Input your dogs age: ")
+
+#2. Convert:
+fAge = float(sAge)
+
+# 3. Compute
+fHumanAge = fAge * 7.3
+
+# 4. Output:
+
+print("The human age equivalent for your dog is: " + str(fHumanAge))
+print("The human age equivalent formatted for your dog is: " + format(fHumanAge,'.1f'))
+      
