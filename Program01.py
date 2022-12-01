dafaaaf5721a6c680cc50c10f1884983ac02ca40
Ray Manzi <70032821+Jamtown40@users.noreply.github.com>
2022-12-01 13:22:14 -0500

    Add files via upload

---
+++ Program01.py
@@ -0,0 +1,36 @@
+# Author Prof C
+# Purpose: Work with print, input, data types and variables.
+
+# print samples
+print("Brian's first code")
+print('Brian first code')
+print("Brian","first","code")
+print("Brian " + "first " + "code")
+
+# Variables
+FirstName = "Brian"
+FirstName2 = "Brian"
+First_Name = "Brian"
+firstname = "Brian"
+FIRSTNAME = "Brian"
+_ThisIsValid = "Steven"
+
+iAge = 17
+print(type(iAge))
+fTaxForCA = 9.8
+print(type(fTaxForCA))
+
+
+iAge = int(22.5)
+print(iAge)
+print(type(iAge))
+
+fAge = float(22)
+print(fAge)
+print(type(fAge))
+
+# Input:
+sName = input("What is your name: ")
+print("Your name is:", sName)
+
+
