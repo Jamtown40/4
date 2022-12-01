951b0f93ee4f7295cab2333a3160e8a239d0165d
Ray M <70032821+Jamtown40@users.noreply.github.com>
2022-11-30 21:58:59 -0500

    Add files via upload

---
+++ FileReadSampleV5.py
@@ -0,0 +1,76 @@
+# This program demonstrates how to read data from a file
+
+# Will read a file and parse out each field. 
+
+def main():
+  try:
+    
+    fileInput = open('FileReadSample.txt', 'r')
+
+    # Read the record and remove the new line character from the end:
+    recInput = fileInput.readline()
+    recInput = recInput.rstrip('\n')
+
+    # Put headings out:
+    outputLine("First Name", "Last Name", "Salary", False)
+    
+    # Process each record in the file until there are no more:
+    while recInput != '' :
+        iBegin = 0
+        iEnd = 0
+        sFirstName = ""
+        sLastName= ""
+        fSalary=0.0
+
+        # Extract/Parse/Substring/Slice each fields
+
+        # get first name:
+        iEnd = recInput.index(',')
+        sFirstName = recInput[iBegin: iEnd]
+
+        # get Last name:
+        iBegin = iEnd + 1
+        iEnd = recInput.index(',' , iBegin)
+        sLastName = recInput[iBegin: iEnd]
+
+        #Get Salary from after the last , and to the end of the string contents:
+        iBegin = iEnd + 1
+        sSalary = recInput[iBegin:]
+
+        # Convert it from string to a float:
+        fSalary = StringToFloat(sSalary)
+        
+        #Output the three separate fields with defined spacing:
+        outputLine(sFirstName, sLastName, fSalary, True)
+    
+        # Read next record and remove the new line character from the end:
+        recInput = fileInput.readline()
+        recInput = recInput.rstrip('\n')
+
+    # Close the file after exiting the loops:
+
+    fileInput.close()
+  except Exception as err:
+      
+      print("General error: " + format(err))
+
+def StringToFloat(sValue):
+  try:
+     fValue = float(sValue)
+  except ValueError:
+     fValue = 0
+     print("Bad data to convert float on: ", sValue)
+
+  return fValue;
+     
+  
+
+def outputLine(sField1, sField2, sField3, blnFormatLastFieldAsNumeric):
+
+    if blnFormatLastFieldAsNumeric :
+        print('{:12}'.format(sField1) + '{:12}'.format(sField2) + '{:10,.2f}'.format(sField3) )
+    else:
+        print('{:12}'.format(sField1) + '{:13}'.format(sField2) + '{:12}'.format(sField3) )
+
+# Program starting point:
+main()
